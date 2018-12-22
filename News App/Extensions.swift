09cf802af0f9634190236287df852a0e0dff31ff
Naveen Gaur <naveengaur800@gmail.com>
2018-12-22 14:03:20 +0530

    Added some extensions to have some extra functionality for UI View, UIColor and autolayout.

---
+++ News App/Extensions.swift
@@ -0,0 +1,72 @@
+//
+//  Extensions.swift
+//  News App
+//
+//  This file contains extensions
+//  for various UI Kit elements
+//  like UI View,UI Colour etc.The
+//  aim is to add additional functionallity
+//  to these elements.
+//
+//  Created by Naveen Gaur on 22/12/18.
+//  Copyright © 2018 Naveen Gaur. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - Extension Methods for UIView.
+extension UIView {
+    
+    /// Allows us to add shadow effect on the UI View.
+    func addShadowEffect(scale: Bool = true) {
+        layer.masksToBounds = false
+        layer.shadowColor = UIColor.lightGray.cgColor
+        layer.shadowOpacity = 0.5
+        layer.shadowOffset = CGSize(width: -1, height: 1)
+        layer.shadowRadius = 5
+        layer.shadowPath = UIBezierPath(rect: bounds).cgPath
+        layer.shouldRasterize = false
+    }
+    
+    /// To Set Auto Layout Constraints on the View.
+    func anchor(top: NSLayoutYAxisAnchor,leading: NSLayoutXAxisAnchor,bottom: NSLayoutYAxisAnchor,trailing: NSLayoutXAxisAnchor,padding: UIEdgeInsets = .zero)
+    {
+        // Activate Auto Layout.
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+        bottomAnchor.constraint(equalTo: bottom, constant: padding.bottom).isActive = true
+        trailingAnchor.constraint(equalTo: trailing, constant: padding.right).isActive = true
+    }
+    
+    /// Increases the border width and changes the UIView color
+    /// to indicate that something has been selected.
+    func highlightBorder()  {
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor().borderColor().cgColor
+        
+    }
+
+}
+
+// MARK: - Extension Methods for UIColor.
+extension UIColor {
+    
+    /// To get Custom Colors or To convert hex code into rgba.
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(netHex:Int) {
+        self.init(red:(netHex >> 16) & 0xff, green:(netHex >> 8) & 0xff, blue:netHex & 0xff)
+    }
+    
+    /// Returns a custom border color configured for this project.
+    func borderColor() -> UIColor {
+        return UIColor.init(netHex: 0xFF2D55)
+    }
+}
